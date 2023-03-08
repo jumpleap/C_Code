c9c11e3cdd7877945c25113e9d213dd6a3f89512
lin-codePlus <972606225@qq.com>
2023-03-08 22:27:27 +0800

    3.7_code

--- test_3_7/test.c
+++ test_3_7/test.c
@@ -55,40 +55,42 @@
 //}
 
 
-void input(double* arr, int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		scanf("%lf", &arr[i]);
-	}
-}
-
-int sort_Double(const void* e1, const void* e2)
-{
-	return *(double*)e1 - *(double*)e2 ? 1 : -1;
-}
-
-void output(double* arr, int len)
-{
-	for (int i = 0; i < len; i++)
-	{
-		printf("%lf ", arr[i]);
-	}
-}
-
-void test2()
-{
-	double arr[4] = { 0 };
-	int len = sizeof(arr) / sizeof(arr[0]);
-	//输入
-	input(arr, len);
-	qsort(arr, len, sizeof(arr[0]), sort_Double);
-	output(arr, len);
-}
-
-int main()
-{
-	//test1();//排序整型数组
-	test2();//排序浮点型数组
-	return 0;
-}
+//void input(double* arr, int len)
+//{
+//	for (int i = 0; i < len; i++)
+//	{
+//		scanf("%lf", &arr[i]);
+//	}
+//}
+//
+//int sort_Double(const void* e1, const void* e2)
+//{
+//	return *(double*)e1 - *(double*)e2 ? 1 : -1;
+//  return (*(double *)a>*(double *)b ? 1:-1)
+//  return (*(double*)b > *(double*)a ? 1 : -1);
+//}
+//
+//void output(double* arr, int len)
+//{
+//	for (int i = 0; i < len; i++)
+//	{
+//		printf("%lf ", arr[i]);
+//	}
+//}
+//
+//void test2()
+//{
+//	double arr[4] = { 0 };
+//	int len = sizeof(arr) / sizeof(arr[0]);
+//	//输入
+//	input(arr, len);
+//	qsort(arr, len, sizeof(arr[0]), sort_Double);
+//	output(arr, len);
+//}
+//
+//int main()
+//{
+//	//test1();//排序整型数组
+//	test2();//排序浮点型数组
+//	return 0;
+//}
